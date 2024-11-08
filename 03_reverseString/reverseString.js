d136964a2869bfc14a4fedd20bb278d53c336e4c
b-adair <76608751+b-adair@users.noreply.github.com>
2024-11-08 08:44:37 -0800

    Implement array redistribution around midpoint

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = string.split("");
+    let midpoint = Math.floor(arr.length / 2);
 
+    for (let i = 0; i < midpoint; i++) {
+        [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
