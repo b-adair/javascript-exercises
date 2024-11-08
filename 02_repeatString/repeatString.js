02eb21f2ea08c6e279f20f76b74d0efa6a338243
b-adair <76608751+b-adair@users.noreply.github.com>
2024-11-08 07:56:11 -0800

    Add solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR";
+    let placeString = "";
+    for (let i = 0; i < num; i++) {
+        placeString += string;
+    }
+    return placeString;
 };
 
 // Do not edit below this line
