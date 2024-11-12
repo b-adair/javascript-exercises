830d29bbdc5c7a3d3070627934d0e83a852a7ac3
b-adair <76608751+b-adair@users.noreply.github.com>
2024-11-11 17:38:43 -0800

    Add script to determine if argument is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisibleByOneHundred = year % 100 === 0;
+    let divisibleByFourHundred = year % 400 === 0;
+    let divisibleByFour = year % 4 === 0;
+
+    if (divisibleByFour && (!divisibleByOneHundred || divisibleByFourHundred)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
