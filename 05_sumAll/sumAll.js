c72de60845f55ffdfcdbea97fea16ea91e22e338
b-adair <76608751+b-adair@users.noreply.github.com>
2024-11-11 16:51:54 -0800

    Implement return sum script
    Returns the sum of all integers between and including the provided
    arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, lastInt) {
+    if (!Number.isInteger(firstInt) || !Number.isInteger(lastInt)) return "ERROR";
+    if (firstInt > lastInt) {
+       [firstInt, lastInt] = [lastInt, firstInt];
+    }
+    if (firstInt < 0 || lastInt < 0) return "ERROR";
+    let sum = 0;
+    for (let i = firstInt; i <= lastInt; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
